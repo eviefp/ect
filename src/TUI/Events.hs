3b2e97cbe0a1911d80458cebb159bb203b892da8
Evie Ciobanu <me@eevie.ro>
2024-07-16 01:54:23 +0300

    tui: use source

--- src/TUI/Events.hs
+++ src/TUI/Events.hs
@@ -1,19 +1,19 @@
 module TUI.Events
-    ( mkEvents
-    , renderEvent
-    , withAttrIf
-    , formatTime
-    , fullTimeFormat
-    , dayFormat
-    , hourFormat
-    ) where
+  ( mkEvents,
+    renderEvent,
+    withAttrIf,
+    formatTime,
+    fullTimeFormat,
+    dayFormat,
+    hourFormat,
+  )
+where
 
 import Brick ((<+>), (<=>))
 import Brick qualified
 import Brick.Widgets.Border qualified as Border
 import Brick.Widgets.List qualified as BL
 import Calendar qualified
-import Data.Foldable (foldl')
 import Data.Sequence (Seq)
 import Data.Text (Text)
 import Data.Time qualified as Time
@@ -32,20 +32,17 @@ hourFormat = "%H:%M"
 
 renderEvent :: Bool -> String -> Bool -> Calendar.Entry -> Brick.Widget Text
 renderEvent isActive format hasFocus Calendar.Entry {..} =
-    withAttrIf (isActive && hasFocus) "highlight" $
-        Brick.txt _entryTitle
-            <=> formatTime format _entryStartTime
-            <=> ( Brick.txt "["
-                    <+> foldl' (<+>) Brick.emptyWidget (Brick.padRight (Brick.Pad 2) . Brick.txt <$> _entryTags)
-                    <+> Brick.txt "]"
-                )
-            <=> Border.hBorder
+  withAttrIf (isActive && hasFocus) "highlight" $
+    Brick.txt _entryTitle
+      <=> formatTime format _entryStartTime
+      <=> (Brick.txt "[" <+> Brick.txt _entrySource <+> Brick.txt "]")
+      <=> Border.hBorder
 
 formatTime :: (Time.FormatTime t) => String -> t -> Brick.Widget n
 formatTime tf = Brick.str . Time.formatTime Time.defaultTimeLocale tf
 
 withAttrIf :: Bool -> String -> Brick.Widget n -> Brick.Widget n
 withAttrIf condition attrName widget =
-    if condition
-        then Brick.withAttr (Brick.attrName attrName) widget
-        else widget
+  if condition
+    then Brick.withAttr (Brick.attrName attrName) widget
+    else widget
